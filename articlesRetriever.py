93138ee7523bd381ca3181dd0355f0491b5c1c37
zoercai <zoercai@gmail.com>
2016-08-13 13:30:44 +1200

    Slight refactoring, renaming to convention and documentation

--- articlesRetriever.py
+++ articlesRetriever.py
@@ -1,10 +1,13 @@
+# Given the date range of the articles to retrieve, retrieves article plain body text from the Guardian API and returns
+#  a list of Article objects.
+
 from urllib.request import urlopen
 import json
 import codecs
 
 api_key = 'aaa8ed1a-d2e0-42f4-9437-11e77e48244b'
 
-query = 'olympics'   # Optional
+query = 'olympics'   # Optional, only used for testing
 fromDate = '2016-07-10'
 toDate = '2016-08-10'
 
